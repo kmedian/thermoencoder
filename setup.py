fec9f2329ed93e7e1c6846ab35de3f13f0316494
UH <554c46@gmail.com>
2019-07-26 10:23:11 +0200

    0.1.1 because typos and flake8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='thermoencoder',
-      version='0.1.0',
+      version='0.1.1',
       description='Thermometer Encoding for ordinal variables',
       long_description=read('README.md'),
       long_description_content_type='text/markdown',
